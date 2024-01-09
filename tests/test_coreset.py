eef14b2590d0c8076c3bbebda739353c7242a7b5
aerdem4 <ahmeterd4@gmail.com>
2024-01-09 14:09:53 +0300

    Add comments for the tests
    Signed-off-by: aerdem4 <ahmeterd4@gmail.com>

--- tests/test_coreset.py
+++ tests/test_coreset.py
@@ -1,8 +1,26 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 1993-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import numpy as np
 from src.coreset import CoresetSampler
 
 
 def test_coreset_outliers():
+    """Test if coreset always includes diverse points (outliers)"""
     n_rows = 1024
     n_cols = 2
     n_samples = n_rows // 8
@@ -22,6 +40,7 @@ def test_coreset_outliers():
 
 
 def test_coreset_determinism():
+    """Test if coreset sampling output is always the same given random seed."""
     n_rows = 1024
     n_cols = 2
     n_samples = n_rows // 8
@@ -42,6 +61,7 @@ def test_coreset_determinism():
 
 
 def test_coreset_std():
+    """Test if coreset sampling is more diverse than random sampling."""
     n_rows = 1024
     n_cols = 2
     n_samples = n_rows // 8
